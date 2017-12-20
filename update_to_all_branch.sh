c147c0277da5803b1fa6f75b032c1f1adaee617a
jsongo <jsongo@qq.com>
2017-12-20 14:58:24 +0800

    update

--- update_to_all_branch.sh
+++ update_to_all_branch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git checkout test && git merge master --no-edit
-git checkout qa && git merge master --no-edit
-git checkout stg && git merge master --no-edit
-git checkout prod && git merge master --no-edit
+git checkout test && git merge master --no-edit && git ps
+git checkout qa && git merge master --no-edit && git ps
+git checkout stg && git merge master --no-edit && git ps
+git checkout prod && git merge master --no-edit && git ps
 git checkout master 
